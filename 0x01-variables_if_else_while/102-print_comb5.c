bb43b1df8f281995e0eb9edf67cf4fb33005410c
happiness kemuel <happinesskemuel@gmail.com>
2022-07-04 18:37:29 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* Return: Always 0
+*main - Prints all possible combinations of single-digit numbers.
 *
-*/ 
+* Return: Always 0.
+*/
 int main(void)
-
 {
-	int num1, num2;
+	int num;
 
-	for (num1 = 0; num1 <= 98; num1++)
-	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 98 && num2 == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+	for (num = 0; num <= 9; num++)
+{
+
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 }
