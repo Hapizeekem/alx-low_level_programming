85dd8772a71b7d2131ff8cf3f5ff23a96fd4e085
happiness kemuel <happinesskemuel@gmail.com>
2022-07-05 14:46:11 -0700

    initial

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*main - program entry point
+*Description : program to print _putchar to the console
+*Return: 0 if program runs successfully
+*/
+int main(void)
+{
+	char putChStr[] = "_putchar";
+	int lengthOfChStr = sizeof(putChStr) / sizeof(putChStr[0]);
+
+	int i;
+
+	for (i = 0; i < lengthOfChStr - 1; i++)
+	{
+		_putchar(putChStr[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
