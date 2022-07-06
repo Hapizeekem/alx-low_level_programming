b248195cd00df72bfb48689443998f8f0c966aba
happiness kemuel <happinesskemuel@gmail.com>
2022-07-06 07:11:22 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main -print _putchar, followed by new line
- * Return; 0
+ * main - print _putchar, followed by new line
+ * Return: 0
  */
 
 int main(void)
@@ -15,7 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,39 +1,24 @@
-#findef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 /**
- * _putchar-writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return On success 1.
- * On error,-1 is returned, and error is set apprioprately.
+ * Return: On success 1.
+ * On error,-1 is returned, and error is set appriopriately.
  */
 
 int _putchar(char c);
 
 /**
- *  print_alphabet - print lowercase alphabet
+ * print_alphabet - print lowercase alphabet
  */
 
 void print_alphabet(void);
 
 /**
- * print_alphabet_x10 - print lowercase alphabet ten times
+ * print_alphabet_x10 - print lowercase ten times
  */
 
-void print_alphabet_10(void);
-/**
- * _islower -checks for lowercase
- *  @c: the character to check
- *  Return: int
- *
- */
-
-int _islower(int c);
-/**
- *  _isalpha -checks for alphabet character
- *   @c: character to check
- *   Return: 1 if lower or uppercase, O if not alphabet character
- *   */
-
-
+#endif
