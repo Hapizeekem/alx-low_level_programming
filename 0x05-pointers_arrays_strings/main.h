c028f114ef9a6e40648fef7a5667bc073d2efc65
happiness kemuel <happinesskemuel@gmail.com>
2022-07-12 18:24:56 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - puts character to standard output
+ *@c: character to put to standard output
+ */
+
 int _putchar(char c);
 
+
 void reset_to_98(int *n);
+
+/**
+ *swap_int - swaps the values of two integers
+ *@a: integer to swap
+ *@b: integer to swap
+ */
+
+void swap_int(int *a, int *b);
+
+
