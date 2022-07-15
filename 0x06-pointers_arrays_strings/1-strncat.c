2b7011df79bfe33fdc23cd1601c2b6297d261167
happiness kemuel <happinesskemuel@gmail.com>
2022-07-14 17:08:20 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	a = 0;
 
-	while (dest[a] != 0)
+	while (dest[a] != '/0')
 	{
 		a++;
 	}
@@ -23,7 +23,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	b = 0;
 
-	while (src[b] != 0 && b < n)
+	while (src[b] != '\0' && b < n)
 	{
 		dest[a] = src[b];
 		a++;
@@ -31,5 +31,5 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 
-	return (dest)
+	return (dest);
 }
