df6ed127b8ec80f2e421d4587c5115a7463c05d0
happiness kemuel <happinesskemuel@gmail.com>
2022-07-23 14:11:38 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,45 +1,13 @@
-#include "main.h"
-
-void print_array_elements(char *str);
-
+#include <stdio.h>
 /**
- * main - Prints all arguments it receives
- *
- * @argc: Length of @argv, integer
- *
- * @argv: Array of strings of the arguments of this program
- *
- * Return: 0, success
+ * main - Prints number of arguments
+ * @argc: argument count
+ * @argv: array of pointers to CLI argument
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		print_array_elements(argv[i]);
-		_putchar('\n');
-	}
-
-	return (0);
-}
-
-/**
- * print_array_elements - Prints all char of a string
- *
- * @str: Pointer to string
- *
- * Return: void
- */
-
-void print_array_elements(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+*argv = *argv;
+printf("%d\n", argc - 1);
+return (0);
 }
