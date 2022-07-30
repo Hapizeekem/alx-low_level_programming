b815c2f037ad42d0ec7d80f78a8544030480e9db
happiness kemuel <happinesskemuel@gmail.com>
2022-07-30 12:43:27 -0700

    100-argstostr.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,56 +1,16 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- *argstostr - function name
- *@ac: first param of type int
- *@av: 2nd param of type char
- *Return: returns a char
- */
-char *argstostr(int ac, char **av)
-{
-	char *new_string = NULL;
-	int k = 0, i = ac, j, sum = 0, temp = 0;
+#include <stdlib.h>
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+void free_grid(int **grid, int height);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int **alloc_grid(int width, int height);
+int len(char *str);
+int num_words(char *str);
 
-	while (ac--)
-		sum += (len(av[ac]) + 1);
-	new_string = (char *) malloc(sum + 1);
-
-	if (new_string != NULL)
-	{
-		while (k < i)
-		{
-			for (j = 0; av[k][j] != '\0'; j++)
-				new_string[j + temp] = av[k][j];
-			new_string[temp + j] = '\n';
-			temp += (j + 1);
-			k++;
-		}
-		new_string[temp] = '\0';
-	}
-	else
-	{
-		return (NULL);
-	}
-	return (new_string);
-}
-
-/**
- *len - gets the length of a string
- *@str: string counnted
- *Return: length of string
- */
-
-int len(char *str)
-{
-	int len = 0;
-
-	if (str != NULL)
-	{
-		while (str[len])
-			len++;
-	}
-	return (len);
-}
+#endif /* MAIN_H */
